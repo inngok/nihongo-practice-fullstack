db7ee527c025bd6d398a8979e069ecca9f0f2ab9
inngok <ngocnguyen120305@gmail.com>
2026-04-25 00:38:08 +0700

    Test CI/CD: Update API URL to production

--- practice-nihongo-frontend/src/api/apiClient.js
+++ practice-nihongo-frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://3.107.17.42/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
